486751b3c5ee1e0d56ad0aa7a918f39f156c30ea
LobbyDiscord <szighub@gmail.com>
2023-10-28 19:41:58 +0200

    New TimeTable System

--- orarend/script.js
+++ orarend/script.js
@@ -8,7 +8,7 @@ fetch('https://api.jsonbin.io/v3/b/65313b1f54105e766fc45c50')
 
         fetch('https://api.jsonbin.io/v3/b/65313b5454105e766fc45c63')
           .then((response) => response.json())
-          .then((exams) => {
+          .then((examinations) => {
 
             var urlParam = new URLSearchParams(window.location.search)
             var paramUser = urlParam.get('name')
@@ -23,76 +23,26 @@ fetch('https://api.jsonbin.io/v3/b/65313b1f54105e766fc45c50')
             document.querySelector(`span.profileName`).textContent = user.name
 
             var days = Object.keys(json.record)
-            var examDays = Object.keys(exams.record)
+            var exams = Object.keys(examinations.record)
+
+            var s = 0
 
             days.forEach((day) => {
-              for (let x = 1; x <= 5; x++) {
-                let z = String.fromCharCode(96 + x)
-                for (let y = 1; y <= 7; y++) {
-                  var id = z + y
-                  if (json.record[day][id]) {
+              for (var x = 1; x <= 5; x++) {
+                for (var y = 1; y <= 7; y++) {
+                  var z = String.fromCharCode(96 + x)
+                  var id = `${z}${y}`
 
-                    examDays.forEach(examDay => {
-                      function getMonday(d) {
-                        d = new Date(d);
-                        var day = x,
-                          diff = d.getDate() - day + (day == 0 ? -6 : 1);
-                        return new Date(d.setDate(diff));
-                      }
+                  exams.forEach((exam) => {
+                    if (json.record[day][id]) {
 
-                      var examDate = getMonday(new Date()).toDateString()
-
-                      if (examDay === examDate) {
-
-                        var p = String.fromCharCode(96 + getMonday(new Date()).getDay())
-
-                        const examData = exams.record[examDay][y]
-                        if (examData) {
-                          var examClass = `${p}${y}`
-                          var examSubject = examData.subject
-                          examIcon(examClass, examSubject)
-                        }
-                        
-                        function examIcon(examClass, examSubject) {
-                        if (examClass && examSubject) {
-                          if (json.record[day][id].subjects[examSubject]) {
-
-                            let cell = document.querySelector(`div#${examClass}`)
-                            let button = cell.parentElement
-                            button.style.cursor = "pointer"
-
-                            const link = document.createElement('a');
-                            link.className = 'examLink'
-                            link.href = examData.link
-                            link.target = "_blank"
-
-                            const image = document.createElement('img');
-                            image.className = 'image';
-                            image.src = "../assets/icon.png"
-
-                            const hover = document.createElement('img');
-                            hover.className = 'hover';
-                            hover.src = "../assets/icon-hover.png"
-
-                            cell.appendChild(image)
-                            cell.appendChild(hover)
-                            button.appendChild(link)
-                          }
-                        }
-                      }
-                      }
-                    })
-
-                    if (json.record[day][id].subjects.multiple) {
-                      var a = "type" + json.record[day][id].subjects.type
-                      var i = user[a]
-                      fill(i)
-                    } else {
                       var i = 1
-                      fill(i)
-                    }
 
-                    function fill(i) {
+                      if (json.record[day][id].subjects.multiple) {
+                        var l = "type" + json.record[day][id].subjects.type
+                        i = user[l]
+                      }
+
                       let cell = document.querySelector(`div#${id}`)
 
                       const teacher = document.createElement('span');
@@ -110,11 +60,132 @@ fetch('https://api.jsonbin.io/v3/b/65313b1f54105e766fc45c50')
                       cell.appendChild(teacher)
                       cell.appendChild(subject)
                       cell.appendChild(classroom)
+
+                      function getMonday(d) {
+                        d = new Date(d);
+                        var value = x,
+                          diff = d.getDate() - d.getDay() + value
+                        return new Date(d.setDate(diff));
+                      }
+
+                      var examDate = getMonday(new Date()).toDateString()
+
+                      if (examDate === exam) {
+
+                        const examData = examinations.record[exam][y]
+                        if (examData) {
+                          var a = day
+                          var b = id
+                          var c = examData.subject
+
+                          if (json.record[a][b].subjects[c]) {
+                            if (json.record[a][b].subjects.multiple) {
+                              var examClass = "type" + json.record[day][id].subjects.type
+                              var userFor = user[examClass]
+                              var examFor = examinations.record[exam][y].subject
+
+                              if (userFor === examFor) {
+
+                                var data = {
+                                  subject: json.record[a][b].subjects[c].subject,
+                                  teacher: json.record[a][b].subjects[c].teacher,
+                                  link: examinations.record[examDate][y].link,
+                                  date: examDate,
+                                  type: examinations.record[examDate][y].type
+                                }
+
+                                s = s + 1
+
+                                let cell = document.querySelector(`div#${id}`)
+                                let button = cell.parentElement
+                                button.style.cursor = "pointer"
+
+                                const link = document.createElement('a');
+                                link.className = 'examLink'
+                                link.href = examData.link
+                                link.target = "_blank"
+
+                                const image = document.createElement('img');
+                                image.className = 'image';
+                                image.src = "../assets/icon.png"
+
+                                const hover = document.createElement('img');
+                                hover.className = 'hover';
+                                hover.src = "../assets/icon-hover.png"
+
+                                cell.appendChild(image)
+                                cell.appendChild(hover)
+                                button.appendChild(link)
+
+
+                                const container = document.querySelector(`div.ticket`)
+
+                                const eventDiv = document.createElement('div');
+                                eventDiv.className = 'event';
+
+                                const spanContainer = document.createElement('div');
+                                spanContainer.className = 'details';
+
+                                const dateSpan = document.createElement('span');
+                                dateSpan.className = 'dateSpan';
+                                dateSpan.textContent = data.date;
+
+                                const linkRow = document.createElement('a')
+                                linkRow.className = 'linkSpan'
+                                linkRow.href = data.link
+
+                                const timeSpan = document.createElement('span');
+                                timeSpan.className = 'timeSpan';
+                                timeSpan.textContent = `${y}. óra`;
+
+                                const typeSpan = document.createElement('span');
+                                typeSpan.className = 'typeSpan';
+                                typeSpan.textContent = data.type
+
+                                const teacherSpan = document.createElement('span');
+                                teacherSpan.className = 'teacherSpan';
+                                teacherSpan.textContent = data.teacher
+
+                                const classSpan = document.createElement('span');
+                                classSpan.className = 'subjectSpan';
+                                classSpan.textContent = data.subject
+
+                                eventDiv.appendChild(linkRow);
+                                linkRow.appendChild(classSpan)
+                                spanContainer.appendChild(dateSpan);
+                                spanContainer.appendChild(teacherSpan);
+                                spanContainer.appendChild(timeSpan);
+                                spanContainer.appendChild(typeSpan);
+
+                                container.appendChild(eventDiv);
+                                eventDiv.appendChild(spanContainer)
+                              }
+                            }
+                          }
+                        }
+                      }
                     }
-                  }
+                  })
                 }
               }
             })
+
+            console.log(s)
+
+            if (s < 1) {
+              const container = document.querySelector(`div.ticket`)
+
+              const eventDiv = document.createElement('div');
+              eventDiv.className = 'event';
+
+              const message = document.createElement('span');
+              message.className = 'message';
+              message.textContent = "Nincsenek dolgozatok ezen a héten"
+
+              container.appendChild(eventDiv);
+              eventDiv.appendChild(message)
+            }
+
           })
       })
   })
